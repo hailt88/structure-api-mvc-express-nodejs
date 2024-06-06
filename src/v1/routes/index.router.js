b1eb0d9357d0099e053b48542920aa9ea529b345
unknown <haithanh.uit@gmail.com>
2024-06-06 11:42:18 +0700

    update timestamp

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const timestamp = Date.now();
 
 router.get('/checkstatus', (req, res, next) => {
     res.status(200).json({
         status: 'success',
-        message: 'api ok add more info there'
+        message: 'api ok add more info there',
+	time: $timestamp
     })
 })
 
